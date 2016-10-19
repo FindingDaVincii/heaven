7c703a804ecfa076141b0dea5353fb814abf2469
Jordan Sleeper <findingDavincii@gmail.com>
2016-10-19 18:51:25 -0400

    mmorpg recode setup

---
+++ views/mmorpg.php
@@ -0,0 +1,11 @@
+<!doctype html><html lang="en"><head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="device=device-width,initial-scale=1.0">
+  <title>ReCode: MMORPG Responsive</title>
+  <link rel="stylesheet" href="../styles/mmorpgstyle.css">
+
+</head><body>
+  
+
+
+</body></html>
