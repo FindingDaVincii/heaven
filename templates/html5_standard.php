dd4f3cd5e7691bf7e541385b8a37b0b682a55d91
Jordan Sleeper <findingDavincii@gmail.com>
2016-10-19 20:25:59 -0400

    trending & networkbar finished for desktop

--- templates/html5_standard.php
+++ templates/html5_standard.php
@@ -1,6 +1,6 @@
 <!doctype html><html lang="en"><head>
   <meta charset="utf-8">
-  <meta name="viewport" content="device=device-width,initial-scale=1.0">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <title>DefaultName</title>
   <link rel="stylesheet" href="../styles/style.css">
 
--- views/mmorpg.php
+++ views/mmorpg.php
@@ -1,11 +1,53 @@
 <!doctype html><html lang="en"><head>
   <meta charset="utf-8">
-  <meta name="viewport" content="device=device-width,initial-scale=1.0">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <title>ReCode: MMORPG Responsive</title>
   <link rel="stylesheet" href="../styles/mmorpgstyle.css">
 
 </head><body>
-  
+  <div class="trending">
+    <div class="wrap margin-center">
+      <strong>Trending</strong> |
+      <a href="#">Revelation</a> |
+      <a href="#">Warhammer 40K: Eternal Crusade</a> |
+      <a href="#">Elder Scrolls Online</a> |
+      <a href="#">Star Wars: The Old Republic</a>
+
+      <div class="socialicons">
+        <a href="#"><img class="icon" src="../img/mmorpg/facebook_icon.gif" alt="" width="12" height="12"/>Facebook</a>
+        <a href="#"><img class="icon" src="../img/mmorpg/twitter_icon.gif" alt="" width="12" height="12"/>Twitter</a>
+        <a href="#"><img class="icon" src="../img/mmorpg/youtube_icon.gif" alt="" width="12" height="12"/>YouTube</a>
+        <a href="#"><img class="icon" src="../img/mmorpg/twitch_icon.gif" alt="" width="12" height="12"/>Twitch.tv</a>
+        <a href="#"><img class="icon" src="../img/mmorpg/youtubeg_icon.gif" alt="" width="12" height="12"/>YouTube.Gaming</a>
+      </div>
+    </div>
+  </div>
+  <div class="networkbar">
+    <div class="wrap margin-center">
+      <form class="network-login" action="index.html" method="post">
+        <strong>Login:</strong>
+        <input type="text" name="username" value="">
+        <strong>Password:</strong>
+        <input type="password" name="password" value="">
+        <strong>Remember?</strong>
+        <input class="icon" type="checkbox" name="remember">
+        <button type="submit" name="network-login-submit">Sign-In</button>
+
+        <div class="register">
+          <a href="#">Register Now</a>
+          | Forgot
+          <a href="#">Username</a>
+          or
+          <a href="#">Password</a>
+        </div>
+      </form>
+
+      <div class="gamejump">
+        <a class="quickgamejump" href="#">Quick Game Jump</a>
+        <a class="rndjump" href="#">Rnd Jump</a>
+      </div>
+    </div>
+  </div>
 
 
 </body></html>
